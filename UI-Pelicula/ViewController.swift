f4bf6f264a991e7ff09f14d9ae34c79e2d0c0c2f
Alumno <ulsa@D109.local>
2022-08-31 08:05:21 -0700

    subir poster

---
+++ UI-Pelicula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI-Pelicula
+//
+//  Created by Alumno on 8/25/22.
+//  Copyright © 2022 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UI-PeliculaTests/UI_PeliculaTests.swift
@@ -0,0 +1,34 @@
+//
+//  UI_PeliculaTests.swift
+//  UI-PeliculaTests
+//
+//  Created by Alumno on 8/25/22.
+//  Copyright © 2022 ULSA. All rights reserved.
+//
+
+import XCTest
+@testable import UI_Pelicula
+
+class UI_PeliculaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
